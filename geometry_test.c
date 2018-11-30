b6b6b242d3dee967d458a349c0732004a996b962
Valyria McFarland <vamc0256@colorado.edu>
2018-11-30 14:01:54 -0700

    mofified .travis.yml

--- geometry_test.c
+++ geometry_test.c
@@ -149,11 +149,11 @@ START_TEST(test_2d_area_triangle) {
 	coord_2d_t a;
 	coord_2d_t b;
 	coord_2d_t c;
-	
+		
 	a.x = b.x = c.x = 0;
 	a.y = b.y = c.y = 0;
 	ck_assert(coord_2d_area_triangle(&a, &b, &c) == 0.0);
-
+	
 	a.x = a.y = 15;
 	b.x = 25;
 	b.y = 30;
