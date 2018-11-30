ba38e6e85afb312c8c7349f84258a1d3dbeb8cf8
Valyria McFarland <vamc0256@colorado.edu>
2018-11-30 14:28:55 -0700

    modified area_triangle

--- geometry.c
+++ geometry.c
@@ -82,7 +82,6 @@ double coord_2d_area_triangle(const coord_2d_t* a, const coord_2d_t* b, const co
 		DEBUG(__FILE__, __LINE__, __func__, "'c' must not be NULL");
 		return NAN;
 	}
-	double num = (a->x*(b->y - c->y) + b->x*(c->y - a->y) + c->x*(a->y - b->y)) / 2.0;
-	return abs(num);
+	return abs((a->x * (b->y - c->y) + b->x * (c->y - a->y) + c->x * (a->y - b->y)) / 2.0);
 
 }
